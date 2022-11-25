fae09b1098de2ca747c31c9ee464eb2bcf33bb31
Sandeep Chaturvedi <schaturvedi@gmail.com>
2022-11-25 15:58:29 -0700

    cleaned up cli tests

--- test/client_cli/en/journey-describe.test.js
+++ test/client_cli/en/journey-describe.test.js
@@ -1,8 +1,5 @@
 import cp from 'child_process';
 import { promisify } from 'util';
-import { node14Compatibility } from '../utils/utils.js';
-
-node14Compatibility();
 
 const exec = promisify(cp.exec);
 const CMD = 'frodo journey describe --help';
