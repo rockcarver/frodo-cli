45b77d919ce38bb52c7e2e302a557caf1fc07f34
Volker Scheuber <volker.scheuber@forgerock.com>
2022-11-26 14:05:39 -0600

    remove ANSI color codes from snapshots and output before comparing

--- test/e2e/info.e2e.test.js
+++ test/e2e/info.e2e.test.js
@@ -8,6 +8,10 @@ const env = {
   env: process.env,
 };
 
+const ansiEscapeCodes =
+  // eslint-disable-next-line no-control-regex
+  /[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g;
+
 const host = 'https://openam-frodo-dev.forgeblocks.com/am';
 const user = 'volker.scheuber@forgerock.com';
 const pass = 'Sup3rS3cr3t!';
@@ -17,18 +21,18 @@ describe('frodo info', () => {
   test('"frodo info": should display cookie name, session id, and access token', async () => {
     const CMD = `frodo info ${host} ${user} ${pass}`;
     const { stderr } = await exec(CMD, env);
-    expect(stderr).toMatchSnapshot();
+    expect(stderr.replace(ansiEscapeCodes, '')).toMatchSnapshot();
   });
 
   test('"frodo info -s": should output cookie name, session id, and access token in json', async () => {
     const CMD = `frodo info -s ${host} ${user} ${pass}`;
     const { stdout } = await exec(CMD, env);
-    expect(stdout).toMatchSnapshot();
+    expect(stdout.replace(ansiEscapeCodes, '')).toMatchSnapshot();
   });
 
   test('"frodo info --scriptFriendly": should output cookie name, session id, and access token in json', async () => {
     const CMD = `frodo info --scriptFriendly ${host} ${user} ${pass}`;
     const { stdout } = await exec(CMD, env);
-    expect(stdout).toMatchSnapshot();
+    expect(stdout.replace(ansiEscapeCodes, '')).toMatchSnapshot();
   });
 });
