b8e104a5d6c18f9b3507e56889d5bad96d34e2ae
Volker Scheuber <volker.scheuber@forgerock.com>
2023-06-14 11:06:52 -0500

    remove frodo-lib destructuring

--- src/cli/service/service-import.ts
+++ src/cli/service/service-import.ts
@@ -9,8 +9,6 @@ import {
 } from '../../ops/ServiceOps.js';
 import { printMessage, verboseMessage } from '../../utils/Console.js';
 
-const { getTokens } = frodo.login;
-
 const program = new FrodoCommand('frodo service import');
 
 interface ServiceImportOptions {
@@ -77,7 +75,11 @@ program
       const globalConfig = options.global ?? false;
 
       // import by id
-      if (options.serviceId && options.file && (await getTokens())) {
+      if (
+        options.serviceId &&
+        options.file &&
+        (await frodo.login.getTokens())
+      ) {
         verboseMessage('Importing service...');
         await importServiceFromFile(
           options.serviceId,
@@ -87,17 +89,17 @@ program
         );
       }
       // -a / --all
-      else if (options.all && options.file && (await getTokens())) {
+      else if (options.all && options.file && (await frodo.login.getTokens())) {
         verboseMessage('Importing all services from a single file...');
         await importServicesFromFile(options.file, clean, globalConfig);
       }
       // -A / --all-separate
-      else if (options.allSeparate && (await getTokens())) {
+      else if (options.allSeparate && (await frodo.login.getTokens())) {
         verboseMessage('Importing all services from separate files...');
         await importServicesFromFiles(clean, globalConfig);
       }
       // import file
-      else if (options.file && (await getTokens())) {
+      else if (options.file && (await frodo.login.getTokens())) {
         verboseMessage('Importing service...');
         await importFirstServiceFromFile(options.file, clean, globalConfig);
       }
