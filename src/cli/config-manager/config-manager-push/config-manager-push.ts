1450b804723e32a13cf7d6ca2866a95c94c07746
Huston Franklin <hfranklin@users.noreply.github.com>
2023-06-25 15:20:06 -0600

    feat: Add config-manager push audit command

--- src/cli/config-manager/config-manager-push/config-manager-push.ts
+++ src/cli/config-manager/config-manager-push/config-manager-push.ts
@@ -1,4 +1,5 @@
 import { FrodoStubCommand } from '../../FrodoCommand';
+import Audit from './config-manager-push-audit';
 import EmailProvider from './config-manager-push-email-provider';
 import EmailTemplates from './config-manager-push-email-templates';
 import Endpoints from './config-manager-push-endpoints';
@@ -27,6 +28,7 @@ export default function setup() {
   program.addCommand(EmailTemplates().name('email-templates'));
   program.addCommand(Schedules().name('schedules'));
   program.addCommand(OrgPrivileges().name('org-privileges'));
+  program.addCommand(Audit().name('audit'));
 
   return program;
 }
