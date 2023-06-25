8997be23f3dd45fbd3e7805a74d84e28e4594bce
Huston Franklin <hfranklin@users.noreply.github.com>
2023-06-25 15:20:06 -0600

    feat: Add config-manager push managed-objects

---
+++ src/cli/config-manager/config-manager-push/config-manager-push-managed-objects.ts
@@ -0,0 +1,59 @@
+import { frodo } from '@rockcarver/frodo-lib';
+import { Option } from 'commander';
+
+import { configManagerImportManagedObjects } from '../../../configManagerOps/FrConfigManagedObjectsOps';
+import { getTokens } from '../../../ops/AuthenticateOps';
+import { printMessage, verboseMessage } from '../../../utils/Console';
+import { FrodoCommand } from '../../FrodoCommand';
+
+const { CLOUD_DEPLOYMENT_TYPE_KEY, FORGEOPS_DEPLOYMENT_TYPE_KEY } =
+  frodo.utils.constants;
+
+const deploymentTypes = [
+  CLOUD_DEPLOYMENT_TYPE_KEY,
+  FORGEOPS_DEPLOYMENT_TYPE_KEY,
+];
+
+export default function setup() {
+  const program = new FrodoCommand(
+    'frodo config-manager push managed-objects',
+    [],
+    deploymentTypes
+  );
+
+  program
+    .description('Import managed objects.')
+    .addOption(
+      new Option(
+        '-n, --name <name>',
+        'Managed object name; imports only the specified managed object.'
+      )
+    )
+    .action(async (host, realm, user, password, options, command) => {
+      command.handleDefaultArgsAndOpts(
+        host,
+        realm,
+        user,
+        password,
+        options,
+        command
+      );
+
+      if (await getTokens(false, true, deploymentTypes)) {
+        verboseMessage('Importing managed objects');
+        const outcome = await configManagerImportManagedObjects(options.name);
+        if (!outcome) process.exitCode = 1;
+      }
+      // unrecognized combination of options or no options
+      else {
+        printMessage(
+          'Unrecognized combination of options or no options...',
+          'error'
+        );
+        program.help();
+        process.exitCode = 1;
+      }
+    });
+
+  return program;
+}
