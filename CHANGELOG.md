9ad317806054ae129a8ffb59c8951693e792f957
Sandeep Chaturvedi <schaturvedi@gmail.com>
2024-01-15 21:57:38 -0700

    added capability for pem and base64hmac encoded ESV secret creation - part of rockcarver/frodo-lib#369 fix

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+### Added
+
+- pem and base64hmac encoded ESV secret creation
+
 ## [2.0.0-44] - 2024-01-11
 
 ## [2.0.0-43] - 2024-01-05
--- src/cli/esv/esv-secret-create.ts
+++ src/cli/esv/esv-secret-create.ts
@@ -1,7 +1,7 @@
 import { frodo } from '@rockcarver/frodo-lib';
 import { Option } from 'commander';
 
-import { createSecret } from '../../ops/SecretsOps';
+import { createSecret, createSecretFromFile } from '../../ops/SecretsOps';
 import { verboseMessage } from '../../utils/Console.js';
 import { FrodoCommand } from '../FrodoCommand';
 
@@ -12,13 +12,18 @@ const program = new FrodoCommand('frodo esv secret create');
 program
   .description('Create secrets.')
   .requiredOption('-i, --secret-id <secret-id>', 'Secret id.')
-  .requiredOption('--value <value>', 'Secret value.')
-  .option('--description [description]', 'Secret description.')
+  .option('--value <value>', 'Secret value. Overrides "--file"')
   .addOption(
     new Option(
-      '--encoding [encoding]',
-      'Secret encoding. Must be one of "generic", "pem", "base64hmac"'
-    ).default('generic', 'generic')
+      '-f, --file [file]',
+      'Name of the file to read pem or base64hmac encoded secret from. Ignored if --value is specified'
+    )
+  )
+  .option('--description [description]', 'Secret description.')
+  .addOption(
+    new Option('--encoding [encoding]', 'Secret encoding')
+      .choices(['generic', 'pem', 'base64hmac'])
+      .default('generic', 'generic')
   )
   .addOption(
     new Option(
@@ -39,13 +44,24 @@ program
       );
       if (await getTokens()) {
         verboseMessage('Creating secret...');
-        const outcome = await createSecret(
-          options.secretId,
-          options.value,
-          options.description,
-          options.encoding,
-          options.useInPlaceholders
-        );
+        let outcome = null;
+        if (options.value) {
+          outcome = await createSecret(
+            options.secretId,
+            options.value,
+            options.description,
+            options.encoding,
+            options.useInPlaceholders
+          );
+        } else {
+          outcome = await createSecretFromFile(
+            options.secretId,
+            options.file,
+            options.description,
+            options.encoding,
+            options.useInPlaceholders
+          );
+        }
         if (!outcome) process.exitCode = 1;
       } else {
         process.exitCode = 1;
