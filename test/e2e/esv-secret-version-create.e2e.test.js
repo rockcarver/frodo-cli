99d354aca7db185bc216118a3e1df31dfb80f5e8
Sandeep Chaturvedi <schaturvedi@gmail.com>
2024-01-19 21:53:44 -0700

    handle encoding when creating a new version of a secret

--- test/e2e/esv-secret-version-create.e2e.test.js
+++ test/e2e/esv-secret-version-create.e2e.test.js
@@ -48,6 +48,8 @@
 
 /*
 FRODO_MOCK=record FRODO_NO_CACHE=1 FRODO_HOST=https://openam-frodo-dev.forgeblocks.com/am frodo esv secret version create -i esv-test-secret-pi-generic --value "3.1"
+FRODO_MOCK=record FRODO_NO_CACHE=1 FRODO_HOST=https://openam-frodo-dev.forgeblocks.com/am frodo esv secret version create -i esv-test-secret-cert-pem-raw --file test/e2e/test-data/esv/key-pair.pem
+FRODO_MOCK=record FRODO_NO_CACHE=1 FRODO_HOST=https://openam-frodo-dev.forgeblocks.com/am frodo esv secret version create --file test/e2e/test-data/esv/hmac-key.txt -i esv-test-secret-file-base64hmac-raw
  */
 import cp from 'child_process';
 import { promisify } from 'util';
@@ -65,9 +67,19 @@ env.env.FRODO_SA_ID = c.saId;
 env.env.FRODO_SA_JWK = c.saJwk;
 
 describe('frodo esv secret version create', () => {
-    test('"frodo esv secret version create -i esv-test-secret-pi-generic --value "3.1"": should create a new version of the "esv-test-var-pi-generic" secret', async () => {
-        const CMD = `frodo esv secret version create -i esv-test-secret-pi-generic --value "3.1"`;
-        const { stdout } = await exec(CMD, env);
-        expect(removeAnsiEscapeCodes(stdout)).toMatchSnapshot();
-    });
+  test('"frodo esv secret version create -i esv-test-secret-pi-generic --value "3.1"": should create a new version of the "esv-test-var-pi-generic" secret', async () => {
+    const CMD = `frodo esv secret version create -i esv-test-secret-pi-generic --value "3.1"`;
+    const { stdout } = await exec(CMD, env);
+    expect(removeAnsiEscapeCodes(stdout)).toMatchSnapshot();
+  });
+  test('"frodo esv secret version create -i esv-test-secret-cert-pem-raw --file test/e2e/test-data/esv/key-pair.pem": should create a new version of the "esv-test-secret-cert-pem-raw" secret', async () => {
+    const CMD = `frodo esv secret version create -i esv-test-secret-cert-pem-raw --file test/e2e/test-data/esv/key-pair.pem`;
+    const { stdout } = await exec(CMD, env);
+    expect(removeAnsiEscapeCodes(stdout)).toMatchSnapshot();
+  });
+  test('"frodo esv secret version create --file test/e2e/test-data/esv/hmac-key.txt -i esv-test-secret-file-base64hmac-raw": should create a new version of the "esv-test-secret-file-base64hmac-raw" secret', async () => {
+    const CMD = `frodo esv secret version create --file test/e2e/test-data/esv/hmac-key.txt -i esv-test-secret-file-base64hmac-raw`;
+    const { stdout } = await exec(CMD, env);
+    expect(removeAnsiEscapeCodes(stdout)).toMatchSnapshot();
+  });
 });
