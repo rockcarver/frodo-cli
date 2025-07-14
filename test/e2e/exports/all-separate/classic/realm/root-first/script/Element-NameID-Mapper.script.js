b4be6d9607b8f5ac79e94c200a3aa1cc620fbf81
Preston Hales <phales@trivir.com>
2025-07-14 17:52:03 -0600

    Update classic exports (for tests) and import tests to pass and work with AM 8.X.X deployments

---
+++ test/e2e/exports/all-separate/classic/realm/root-first/script/Element-NameID-Mapper.script.js
@@ -0,0 +1,1 @@
+identity.getAttributeValues("uid")[0];
--- test/e2e/exports/all-separate/classic/realm/root-first/script/LinkedIn-Profile-Normalization.script.groovy
+++ test/e2e/exports/all-separate/classic/realm/root-first/script/LinkedIn-Profile-Normalization.script.groovy
@@ -1,19 +1,23 @@
 /*
- * Copyright 2020 ForgeRock AS. All Rights Reserved
+ * Copyright 2024-2025 Ping Identity Corporation. All Rights Reserved
  *
- * Use of this code requires a commercial software license with ForgeRock AS.
- * or with one of its affiliates. All use shall be exclusively subject
- * to such license between the licensee and ForgeRock AS.
+ * This code is to be used exclusively in connection with Ping Identity
+ * Corporation software or services. Ping Identity Corporation only offers
+ * such software or services to legal entities who have entered into a
+ * binding license agreement with Ping Identity Corporation.
  */
+
 
 import static org.forgerock.json.JsonValue.field
 import static org.forgerock.json.JsonValue.json
 import static org.forgerock.json.JsonValue.object
 
 return json(object(
-        field("id", rawProfile.id),
-        field("givenName", rawProfile.firstName.localized.get(0)),
-        field("familyName", rawProfile.lastName.localized.get(0)),
-        field("photoUrl", rawProfile.profilePicture.displayImage),
-        field("email", rawProfile.elements.get(0).get("handle~").emailAddress),
-        field("username", rawProfile.elements.get(0).get("handle~").emailAddress)))
+        field("id", rawProfile.sub),
+        field("displayName", rawProfile.name),
+        field("givenName", rawProfile.given_name),
+        field("familyName", rawProfile.family_name),
+        field("photoUrl", rawProfile.picture),
+        field("email", rawProfile.email),
+        field("emailVerified", rawProfile.email_verified),
+        field("username", rawProfile.email)))
