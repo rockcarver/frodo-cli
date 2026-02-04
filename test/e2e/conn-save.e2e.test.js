6b2799235c5c450e296083b88cbbf36dc4dc74c7
phalestrivir <139594093+phalestrivir@users.noreply.github.com>
2026-02-03 17:22:02 -0700

    Fix conn save and conn describe tests (#535)
    * Fix conn save and conn describe tests
    
    * Run audit fix

--- test/e2e/conn-save.e2e.test.js
+++ test/e2e/conn-save.e2e.test.js
@@ -48,7 +48,7 @@
 import cp from 'child_process';
 import { promisify } from 'util';
 import {getEnv, removeAnsiEscapeCodes, testif} from './utils/TestUtils';
-import { connection as c, classic_connection as cc } from './utils/TestConfig';
+import { connection as c, amster_connection as cc } from './utils/TestConfig';
 import { writeFileSync, rmSync } from 'fs';
 
 const exec = promisify(cp.exec);
@@ -58,22 +58,26 @@ const connectionsSaveFile = './test/e2e/env/ConnectionsSave.json';
 process.env['FRODO_MOCK'] = '1';
 process.env['FRODO_CONNECTION_PROFILES_PATH'] =
   './test/e2e/env/ConnectionsSave.json';
-const env = getEnv();
+const env = getEnv(c);
+const classicEnv = getEnv(cc);
 
 const jwkFile = 'test/fs_tmp/conn-save-jwk.json';
+const pkFile = 'test/fs_tmp/conn-save-pk';
 
 beforeAll(() => {
   writeFileSync(jwkFile, c.saJwk);
+  writeFileSync(pkFile, cc.pk);
   writeFileSync(connectionsSaveFile, '{}');
 });
 
 afterAll(() => {
   rmSync(jwkFile);
+  rmSync(pkFile);
   rmSync(connectionsSaveFile);
 });
 
 describe('frodo conn save', () => {
-  testif(process.env['FRODO_MASTER_KEY'])(
+  testif(process.env['FRODO_MASTER_KEY'] || process.env['FRODO_MASTER_KEY_PATH'])(
     `"frodo conn save --no-validate ${c.host} ${c.user} ${c.pass}": save new connection profile using an admin account.`,
     async () => {
       const CMD = `frodo conn save --no-validate ${c.host} ${c.user} ${c.pass}`;
@@ -82,7 +86,7 @@ describe('frodo conn save', () => {
     }
   );
 
-  testif(process.env['FRODO_MASTER_KEY'])(
+  testif(process.env['FRODO_MASTER_KEY'] || process.env['FRODO_MASTER_KEY_PATH'])(
     `"frodo conn save --no-validate --sa-id ${c.saId} --sa-jwk-file ${jwkFile} ${c.host}": save new connection profile with existing service account and without admin account.`,
     async () => {
       const CMD = `frodo conn save --no-validate --sa-id ${c.saId} --sa-jwk-file ${jwkFile} ${c.host}`;
@@ -91,20 +95,20 @@ describe('frodo conn save', () => {
     }
   );
 
-  testif(process.env['FRODO_MASTER_KEY'])(
+  testif(process.env['FRODO_MASTER_KEY'] || process.env['FRODO_MASTER_KEY_PATH'])(
     `"frodo conn save --no-validate ${cc.host} ${cc.user} ${cc.pass}": save new classic connection profile using an admin account.`,
     async () => {
       const CMD = `frodo conn save --no-validate ${cc.host} ${cc.user} ${cc.pass}`;
-      const { stderr } = await exec(CMD, env);
+      const { stderr } = await exec(CMD, classicEnv);
       expect(removeAnsiEscapeCodes(stderr)).toMatchSnapshot();
     }
   );
 
-  testif(process.env['FRODO_MASTER_KEY'])(
-    `"frodo conn save --no-validate --private-key ${cc.pk} --authentication-service ${cc.authService} ${cc.host}": save new classic connection profile with private key and custom authentication service.`,
+  testif(process.env['FRODO_MASTER_KEY'] || process.env['FRODO_MASTER_KEY_PATH'])(
+    `"frodo conn save --no-validate --private-key ${pkFile} --authentication-service ${cc.authService} ${cc.host}": save new classic connection profile with private key and custom authentication service.`,
     async () => {
-      const CMD = `frodo conn save --no-validate --private-key ${cc.pk} --authentication-service ${cc.authService} ${cc.host}`;
-      const { stderr } = await exec(CMD, env);
+      const CMD = `frodo conn save --no-validate --private-key ${pkFile} --authentication-service ${cc.authService} ${cc.host}`;
+      const { stderr } = await exec(CMD, classicEnv);
       expect(removeAnsiEscapeCodes(stderr)).toMatchSnapshot();
     }
   );
