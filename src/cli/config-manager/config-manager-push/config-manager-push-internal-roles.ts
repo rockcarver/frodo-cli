dd0d9a8661254010a1ec9cfeca2011b9c6259e60
dallinjsevy <dsevy@trivir.com>
2026-03-26 22:46:51 -0600

    feat: Add config-manager push internal-roles command (#567)
    * feat: Add config-manager push internal-roles command
    
    * Update to frodo-lib 4.0.0-32
    
    ---------
    
    Co-authored-by: Volker Scheuber <vscheuber@gmail.com>

---
+++ src/cli/config-manager/config-manager-push/config-manager-push-internal-roles.ts
@@ -0,0 +1,60 @@
+import { frodo } from '@rockcarver/frodo-lib';
+import { Option } from 'commander';
+
+import { configManagerImportInternalRoles } from '../../../configManagerOps/FrConfigInternalRolesOps';
+import { getTokens } from '../../../ops/AuthenticateOps';
+import { printMessage, verboseMessage } from '../../../utils/Console';
+import { FrodoCommand } from '../../FrodoCommand';
+
+const { CLOUD_DEPLOYMENT_TYPE_KEY, FORGEOPS_DEPLOYMENT_TYPE_KEY } =
+  frodo.utils.constants;
+
+const deploymentTypes = [
+  CLOUD_DEPLOYMENT_TYPE_KEY,
+  FORGEOPS_DEPLOYMENT_TYPE_KEY,
+];
+
+export default function setup() {
+  const program = new FrodoCommand(
+    'frodo config-manager push internal-roles',
+    [],
+    deploymentTypes
+  );
+
+  program
+
+    .description('Import internal roles.')
+    .addOption(
+      new Option(
+        '-n, --name <name>',
+        'Internal role name, imports the specified role name'
+      )
+    )
+    .action(async (host, realm, user, password, options, command) => {
+      command.handleDefaultArgsAndOpts(
+        host,
+        realm,
+        user,
+        password,
+        options,
+        command
+      );
+
+      if (await getTokens(false, true, deploymentTypes)) {
+        verboseMessage('Importing internal roles');
+        const outcome = await configManagerImportInternalRoles(options.name);
+        if (!outcome) process.exitCode = 1;
+      }
+      // unrecognized combination of options or no options
+      else {
+        printMessage(
+          'Unrecognized combination of options or no options...',
+          'error'
+        );
+        program.help();
+        process.exitCode = 1;
+      }
+    });
+
+  return program;
+}
--- src/cli/config-manager/config-manager-push/config-manager-push.ts
+++ src/cli/config-manager/config-manager-push/config-manager-push.ts
@@ -1,6 +1,7 @@
 import { FrodoStubCommand } from '../../FrodoCommand';
 import EmailProvider from './config-manager-push-email-provider';
 import Endpoints from './config-manager-push-endpoints';
+import InternalRoles from './config-manager-push-internal-roles';
 import Kba from './config-manager-push-kba';
 import Locales from './config-manager-push-locales';
 import PasswordPolicy from './config-manager-push-password-policy';
@@ -19,6 +20,6 @@ export default function setup() {
   program.addCommand(EmailProvider().name('email-provider'));
   program.addCommand(Endpoints().name('endpoints'));
   program.addCommand(Kba().name('kba'));
-
+  program.addCommand(InternalRoles().name('internal-roles'));
   return program;
 }
