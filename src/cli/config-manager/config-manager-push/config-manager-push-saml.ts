2731e95093bdf0357ce85df51bb6ae5e6d2cba6b
Dallin Sevy <dsevy@trivir.com>
2026-04-07 12:53:34 -0600

    feat: Add config-manager push saml command

---
+++ src/cli/config-manager/config-manager-push/config-manager-push-saml.ts
@@ -0,0 +1,34 @@
+import { Option } from 'commander';
+
+import { configManagerImportSaml } from '../../../configManagerOps/FrConfigSamlOps';
+import { getTokens } from '../../../ops/AuthenticateOps';
+import { verboseMessage } from '../../../utils/Console';
+import { FrodoCommand } from '../../FrodoCommand';
+
+export default function setup() {
+  const program = new FrodoCommand('frodo config-manager push saml');
+  program
+    .description('Import saml configuration.')
+    .addOption(
+      new Option(
+        '-n, --name <name>',
+        'The entityId of a single SAML entity to import.'
+      )
+    )
+    .action(async (host, realm, user, password, options, command) => {
+      command.handleDefaultArgsAndOpts(
+        host,
+        realm,
+        user,
+        password,
+        options,
+        command
+      );
+      const getTokensIsSuccessful = await getTokens();
+      if (!getTokensIsSuccessful) process.exit(1);
+      verboseMessage('Importing SAML configuration.');
+      const outcome = await configManagerImportSaml(realm, options.name);
+      if (!outcome) process.exitCode = 1;
+    });
+  return program;
+}
---
+++ test/e2e/config-manager-push-saml.e2e.test.js
@@ -0,0 +1,89 @@
+/**
+ * Follow this process to write e2e tests for the CLI project:
+ *
+ * 1. Test if all the necessary mocks for your tests already exist.
+ *    In mock mode, run the command you want to test with the same arguments
+ *    and parameters exactly as you want to test it, for example:
+ *
+ *    $ FRODO_MOCK=1 frodo conn save https://openam-frodo-dev.forgeblocks.com/am volker.scheuber@forgerock.com Sup3rS3cr3t!
+ *
+ *    If your command completes without errors and with the expected results,
+ *    all the required mocks already exist and you are good to write your
+ *    test and skip to step #4.
+ *
+ *    If, however, your command fails and you see errors like the one below,
+ *    you know you need to record the mock responses first:
+ *
+ *    [Polly] [adapter:node-http] Recording for the following request is not found and `recordIfMissing` is `false`.
+ *
+ * 2. Record mock responses for your exact command.
+ *    In mock record mode, run the command you want to test with the same arguments
+ *    and parameters exactly as you want to test it, for example:
+ *
+ *    $ FRODO_MOCK=record frodo conn save https://openam-frodo-dev.forgeblocks.com/am volker.scheuber@forgerock.com Sup3rS3cr3t!
+ *
+ *    Wait until you see all the Polly instances (mock recording adapters) have
+ *    shutdown before you try to run step #1 again.
+ *    Messages like these indicate mock recording adapters shutting down:
+ *
+ *    Polly instance 'conn/4' stopping in 3s...
+ *    Polly instance 'conn/4' stopping in 2s...
+ *    Polly instance 'conn/save/3' stopping in 3s...
+ *    Polly instance 'conn/4' stopping in 1s...
+ *    Polly instance 'conn/save/3' stopping in 2s...
+ *    Polly instance 'conn/4' stopped.
+ *    Polly instance 'conn/save/3' stopping in 1s...
+ *    Polly instance 'conn/save/3' stopped.
+ *
+ * 3. Validate your freshly recorded mock responses are complete and working.
+ *    Re-run the exact command you want to test in mock mode (see step #1).
+ *
+ * 4. Write your test.
+ *    Make sure to use the exact command including number of arguments and params.
+ *
+ * 5. Commit both your test and your new recordings to the repository.
+ *    Your tests are likely going to reside outside the frodo-lib project but
+ *    the recordings must be committed to the frodo-lib project.
+ */
+
+/*
+// ForgeOps
+FRODO_MOCK=record FRODO_NO_CACHE=1 FRODO_HOST=https://nightly.gcp.forgeops.com/am frodo config-manager push saml --env-file test/e2e/env/configManager1.env -D test/e2e/exports/fr-config-manager/forgeops/ -m forgeops
+FRODO_MOCK=record FRODO_NO_CACHE=1 FRODO_HOST=https://nightly.gcp.forgeops.com/am frodo config-manager push saml -n test-IDP -E IDP_URL_PLACEHOLDER=https://platform.dev.trivir.com/am/idpsaehandler/metaAlias/alpha/test -E HTTP_REDIRECT_PLACEHOLDER=urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect -D test/e2e/exports/fr-config-manager/forgeops -m forgeops
+FRODO_MOCK=record FRODO_NO_CACHE=1 FRODO_HOST=https://nightly.gcp.forgeops.com/am frodo config-manager push saml --env-file test/e2e/env/configManager1.env --directory test/e2e/exports/fr-config-manager/forgeops/ -m forgeops
+*/
+
+import { getEnv, testSuccess } from './utils/TestUtils';
+import { forgeops_connection as fc } from './utils/TestConfig';
+
+process.env['FRODO_MOCK'] = '1';
+const forgeopsEnv = getEnv(fc);
+
+const allDirectory = "test/e2e/exports/fr-config-manager/forgeops";
+const envDir = "test/e2e/env/configManager1.env"
+
+describe('frodo config-manager push saml', () => {
+    test(`"frodo config-manager push saml --env-file ${envDir} -D ${allDirectory} -m forgeops": should import the saml into forgeops"`, async () => {
+        const CMD = `frodo config-manager push saml --env-file ${envDir} -D ${allDirectory} -m forgeops`;
+        await testSuccess(CMD, forgeopsEnv)
+    });
+    test(`"frodo config-manager push saml -n test-IDP -E IDP_URL_PLACEHOLDER=https://platform.dev.trivir.com/am/idpsaehandler/metaAlias/alpha/test6 -E HTTP_REDIRECT_PLACEHOLDER=urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect -D ${allDirectory} -m forgeops": should import a saml entity and update placeholders`, async () => {
+        const CMD = `frodo config-manager push saml -n test-IDP -E IDP_URL_PLACEHOLDER=https://platform.dev.trivir.com/am/idpsaehandler/metaAlias/alpha/test -E HTTP_REDIRECT_PLACEHOLDER=urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect -D ${allDirectory} -m forgeops`;
+        await testSuccess(CMD, {
+            env: {
+                ...forgeopsEnv.env,
+                FRODO_REALM: "alpha"
+            }
+        });
+    });
+    test(`"frodo config-manager push saml --env-file ${envDir} --directory ${allDirectory} -m forgeops": should import the saml into forgeops alpha realm"`, async () => {
+        const CMD = `frodo config-manager push saml --env-file ${envDir} --directory ${allDirectory} -m forgeops`;
+        await testSuccess(CMD, {
+            env: {
+                ...forgeopsEnv.env,
+                FRODO_REALM: "alpha"
+            }
+        });
+        
+    });
+});
