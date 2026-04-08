e238cc255cd2625f5edd01c0b01ec3ef5fbaad26
dallinjsevy <dsevy@trivir.com>
2026-04-08 08:43:08 -0600

    feat: Add config-manager push cookie-domains command (#583)
    Co-authored-by: Huston Franklin <hfranklin@users.noreply.github.com>

---
+++ src/cli/config-manager/config-manager-push/config-manager-push-cookie-domain.ts
@@ -0,0 +1,52 @@
+import { frodo } from '@rockcarver/frodo-lib';
+
+import { configManagerImportCookieDomains } from '../../../configManagerOps/FrConfigCookieDomainsOps';
+import { getTokens } from '../../../ops/AuthenticateOps';
+import { printMessage, verboseMessage } from '../../../utils/Console';
+import { FrodoCommand } from '../../FrodoCommand';
+
+const { CLOUD_DEPLOYMENT_TYPE_KEY, FORGEOPS_DEPLOYMENT_TYPE_KEY } =
+  frodo.utils.constants;
+
+const deploymentTypes = [
+  CLOUD_DEPLOYMENT_TYPE_KEY,
+  FORGEOPS_DEPLOYMENT_TYPE_KEY,
+];
+
+export default function setup() {
+  const program = new FrodoCommand(
+    'frodo config-manager push cookie-domains',
+    [],
+    deploymentTypes
+  );
+
+  program
+    .description('Import cookie domains.')
+    .action(async (host, realm, user, password, options, command) => {
+      command.handleDefaultArgsAndOpts(
+        host,
+        realm,
+        user,
+        password,
+        options,
+        command
+      );
+
+      if (await getTokens(false, true, deploymentTypes)) {
+        verboseMessage('Importing cookie domains...');
+        const outcome = await configManagerImportCookieDomains();
+        if (!outcome) process.exitCode = 1;
+      }
+      // unrecognized combination of options or no options
+      else {
+        printMessage(
+          'Unrecognized combination of options or no options...',
+          'error'
+        );
+        program.help();
+        process.exitCode = 1;
+      }
+    });
+
+  return program;
+}
