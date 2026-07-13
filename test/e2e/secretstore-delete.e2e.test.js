048bb6d6dc223e7c488314737454b0c366193f32
Volker Scheuber <vscheuber@gmail.com>
2026-07-13 12:12:31 -0600

    Update secretstore-delete tests, recordings, and snapshots.

--- test/e2e/secretstore-delete.e2e.test.js
+++ test/e2e/secretstore-delete.e2e.test.js
@@ -63,28 +63,32 @@ import { connection as c, classic_connection as cc } from './utils/TestConfig';
 process.env['FRODO_MOCK'] = '1';
 process.env['FRODO_CONNECTION_PROFILES_PATH'] =
     './test/e2e/env/Connections.json';
-const env = getEnv(c);
+const cloudEnv = getEnv(c);
 const classicEnv = getEnv(cc);
 
 describe('frodo secretstore delete', () => {
-    test('"frodo secretstore delete -i ESV": should fail deleting ESV secret store', async () => {
-        const CMD = `frodo secretstore delete -i ESV`;
-        await testFail(CMD, env);
+    describe('Cloud', () => {
+        test('"frodo secretstore delete -i ESV": should fail deleting ESV secret store', async () => {
+            const CMD = `frodo secretstore delete -i ESV`;
+            await testFail(CMD, cloudEnv);
+        });
+        test('"frodo secretstore delete --all": should fail deleting any secret store', async () => {
+            const CMD = `frodo secretstore delete --all`;
+            await testFail(CMD, cloudEnv);
+        });
     });
-    test('"frodo secretstore delete --all": should fail deleting any secret store', async () => {
-        const CMD = `frodo secretstore delete --all`;
-        await testFail(CMD, env);
-    });
-    test('"frodo secretstore delete -g --secretstore-id EnvironmentAndSystemPropertySecretStore --secretstore-type EnvironmentAndSystemPropertySecretStore -m classic": should fail deleting EnvironmentAndSystemPropertySecretStore', async () => {
-        const CMD = `frodo secretstore delete -g --secretstore-id EnvironmentAndSystemPropertySecretStore --secretstore-type EnvironmentAndSystemPropertySecretStore -m classic`;
-        await testFail(CMD, classicEnv);
-    });
-    test('"frodo secretstore delete --global -i default-keystore --type classic": should delete the global default keystore', async () => {
-        const CMD = `frodo secretstore delete --global -i default-keystore --type classic`;
-        await testSuccess(CMD, classicEnv);
-    });
-    test('"frodo secretstore delete -agm classic": should delete all global keystores with exception of EnvironmentAndSystemPropertySecretStore', async () => {
-        const CMD = `frodo secretstore delete -agm classic`;
-        await testFail(CMD, classicEnv);
+    describe('Classic', () => {
+        test('"frodo secretstore delete -g --secretstore-id EnvironmentAndSystemPropertySecretStore --secretstore-type EnvironmentAndSystemPropertySecretStore -m classic": should fail deleting EnvironmentAndSystemPropertySecretStore', async () => {
+            const CMD = `frodo secretstore delete -g --secretstore-id EnvironmentAndSystemPropertySecretStore --secretstore-type EnvironmentAndSystemPropertySecretStore -m classic`;
+            await testFail(CMD, classicEnv);
+        });
+        test('"frodo secretstore delete --global -i default-keystore --type classic": should delete the global default keystore', async () => {
+            const CMD = `frodo secretstore delete --global -i default-keystore --type classic`;
+            await testSuccess(CMD, classicEnv);
+        });
+        test('"frodo secretstore delete -agm classic": should delete all global keystores with exception of EnvironmentAndSystemPropertySecretStore', async () => {
+            const CMD = `frodo secretstore delete -agm classic`;
+            await testFail(CMD, classicEnv);
+        });
     });
 });
