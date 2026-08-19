20e0a5c904f2dcbb65ed7cc55e7a8ce9dd0a965b
Dallin Sevy <dsevy@trivir.com>
2026-08-19 08:28:36 -0600

    fixup! wip

---
+++ src/configManagerOps/FrConfigMetadataOps.ts
@@ -0,0 +1,47 @@
+import { frodo } from '@rockcarver/frodo-lib';
+import fs from 'fs';
+
+import { printError } from '../utils/Console';
+
+const { readConfigEntity, importConfigEntities } = frodo.idm.config;
+const { getFilePath, saveJsonToFile } = frodo.utils;
+
+/**
+ * Export Idm authentication configuration in fr-config-manager format.
+ * @return {Promise<boolean>} a promise that resolves to true if successful, false otherwise
+ */
+export async function configManagerExportIdmAuthentication(): Promise<boolean> {
+  try {
+    const exportData = await readConfigEntity('metadata');
+    saveJsonToFile(
+      exportData,
+      getFilePath('idm-authentication-config/authentication.json', true),
+      false
+    );
+    return true;
+  } catch (error) {
+    printError(error, `Error exporting config entity selfservice.kba`);
+  }
+  return false;
+}
+
+/**
+ * Import Idm authentication configuration in fr-config-manager format.
+ * @return {Promise<boolean>} a promise that resolves to true if successful, false otherwise
+ */
+export async function configManagerImportIdmAuthentication(): Promise<boolean> {
+  try {
+    const filePath = getFilePath('idm-authentication-config');
+    const fileData = fs.readFileSync(
+      `${filePath}/authentication.json`,
+      'utf-8'
+    );
+    let importData = JSON.parse(fileData);
+    importData = { idm: { [importData._id]: importData } };
+    await importConfigEntities(importData);
+    return true;
+  } catch (error) {
+    printError(error);
+  }
+  return false;
+}
